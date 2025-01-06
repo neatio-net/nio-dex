667953e85a0a5366075acf4cca098db223f5d1a6
MrTizu <99123684+tizul@users.noreply.github.com>
2025-01-06 08:24:21 +0200

    Create CollectRoundWinningsModal.tsx

---
+++ src/views/Predictions/components/CollectRoundWinningsModal.tsx
@@ -0,0 +1,171 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import {
+  ModalContainer,
+  ModalBody,
+  ModalTitle,
+  ModalHeader,
+  InjectedModalProps,
+  Button,
+  AutoRenewIcon,
+  TrophyGoldIcon,
+  Text,
+  Flex,
+  Heading,
+  Box,
+  LinkExternal,
+  ModalCloseButton,
+} from '@zaigar-finance/uikit'
+import Cookies from 'js-cookie'
+import { useWeb3React } from '@web3-react/core'
+import { getBscScanLink } from 'utils'
+import { useAppDispatch } from 'state'
+import { usePriceBnbBusd , usePriceZaifBusd } from 'state/farms/hooks'
+import { fetchClaimableStatuses } from 'state/predictions'
+import { useTranslation } from 'contexts/Localization'
+import useToast from 'hooks/useToast'
+import { usePredictionsContract,useReferral } from 'hooks/useContract'
+
+interface CollectRoundWinningsModalProps extends InjectedModalProps {
+  payout: string
+  betAmount: string
+  epoch: number
+  onSuccess?: () => Promise<void>
+}
+
+const Modal = styled(ModalContainer)`
+  overflow: visible;
+`
+
+const PrizeDecoration = styled.div`
+  position: absolute;
+  top: -116px; // line up bunny at the top of the modal
+  left: 0px;
+  text-align: center;
+  width: 100%;
+`
+
+const CollectRoundWinningsModal: React.FC<CollectRoundWinningsModalProps> = ({
+  payout,
+  betAmount,
+  epoch,
+  onDismiss,
+  onSuccess,
+}) => {
+  const [isPendingTx, setIsPendingTx] = useState(false)
+  const [isReferredAddress, setReferralAddress] = useState("0x")
+  const { account } = useWeb3React()
+  const { t } = useTranslation()
+  const { toastSuccess, toastError } = useToast()
+  const predictionsContract = usePredictionsContract()
+  const referralContract = useReferral()
+  const bnbBusdPrice = usePriceBnbBusd()
+  const zaifBusdPrice = usePriceZaifBusd()
+  const dispatch = useAppDispatch()
+ 
+  // Convert payout to number for compatibility
+  const payoutAsFloat = parseFloat(payout)
+  const betAmountAsFloat = parseFloat(betAmount)
+
+
+  const handleClick = async () => {
+    try {
+      const tx = await predictionsContract.claim([epoch])
+      setIsPendingTx(true)
+      const receipt = await tx.wait()
+
+      if (onSuccess) {
+        await onSuccess()
+      }
+
+      await dispatch(fetchClaimableStatuses({ account, epochs: [epoch] }))
+      onDismiss()
+      setIsPendingTx(false)
+      toastSuccess(
+        t('Winnings collected!'),
+        <Box>
+          <Text as="p" mb="8px">
+            {t('Your prizes have been sent to your wallet')}
+          </Text>
+          {receipt.transactionHash && (
+            <LinkExternal href={getBscScanLink(receipt.transactionHash, 'transaction')}>
+              {t('View on BscScan')}
+            </LinkExternal>
+          )}
+        </Box>,
+      )
+    } catch (error) {
+      toastError(
+        t('Error'),
+        error?.data?.message || t('Please try again. Confirm the transaction and make sure you are paying enough gas!'),
+      )
+    } finally {
+      setIsPendingTx(false)
+    }
+  }
+
+  const getReferral = async () => { 
+    try {
+      const response = await referralContract.getReferrer(account)
+      setReferralAddress(response)
+      return response
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+  getReferral();
+  // Get Referral Address
+  const getreferrer = Cookies.get("ReferAddress");
+  const referrerAddress = getreferrer.replace(/"/g,"");
+
+  const hasReferred = isReferredAddress !== "0x0000000000000000000000000000000000000000" || referrerAddress !== "0x0000000000000000000000000000000000000000";
+  return (
+    <Modal minWidth="288px" position="relative" mt="124px">
+      <PrizeDecoration>
+        <img src="/images/decorations/prize-trophy.png" alt="prize decoration" height="124px" width="168px" />
+      </PrizeDecoration>
+      <ModalHeader>
+        <ModalTitle>
+          <Heading>{t('Collect Winnings')}</Heading>
+        </ModalTitle>
+        <ModalCloseButton onDismiss={onDismiss} />
+      </ModalHeader>
+      <ModalBody p="24px">
+        <Flex alignItems="start" justifyContent="space-between" mb="8px">
+          <Text>{t('Your position')}</Text>
+          <Box style={{ textAlign: 'right' }}>
+            <Text>{`${betAmount} ZAIF`}</Text>
+            <Text fontSize="12px" color="textSubtle">
+              {`~$${zaifBusdPrice.times(betAmountAsFloat).toFormat(2)}`}
+            </Text>
+          </Box>
+        </Flex>
+        <Flex alignItems="start" justifyContent="space-between" mb="24px">
+          <Text>{t('Your winnings')}</Text>
+          <Box style={{ textAlign: 'right' }}>
+            <Text>{`${payout} ZAIF`}</Text>
+            <Text fontSize="12px" color="textSubtle">
+              {`~$${zaifBusdPrice.times(payoutAsFloat).toFormat(2)}`}
+            </Text>
+            <Text fontSize="10px" color="textSubtle"> Payouts showing here are not accounting 2% fees from (Pot/Referral) and Zaif transfer Fees (5%)</Text>
+          </Box>
+        </Flex>
+        {hasReferred && (
+            <Text fontSize="12px" color="secondary">You are referred by: {referrerAddress !== "0x0000000000000000000000000000000000000000" ? referrerAddress : isReferredAddress}</Text>
+           )}
+        <Button
+          width="100%"
+          mb="8px"
+          onClick={handleClick}
+          isLoading={isPendingTx}
+          endIcon={isPendingTx ? <AutoRenewIcon spin color="currentColor" /> : null}
+        >
+          {t('Confirm')}
+        </Button>
+      </ModalBody>
+    </Modal>
+  )
+}
+
+export default CollectRoundWinningsModal
